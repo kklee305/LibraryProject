180b4c2aeb3df5bd8c270af57ba55a95d15b3ea0
kklee305 <kklee305@gmail.com>
2015-05-30 18:40:26 -0400

    platform list view

--- app/src/main/java/ca/kklee/libraryproject/HomeActivity.java
+++ app/src/main/java/ca/kklee/libraryproject/HomeActivity.java
@@ -1,19 +1,51 @@
 package ca.kklee.libraryproject;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import ca.kklee.libraryproject.ImageViewPlatform.ImageViewPlatform;
+import ca.kklee.libraryproject.LoggerPlatform.LoggerPlatform;
 
 public class HomeActivity extends ActionBarActivity {
+
+    private String[] platformList = {"ImageViewPlatform", "Logging"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        InitPlatformListView();
     }
 
+    private void InitPlatformListView() {
+        ListView drawerList = (ListView) findViewById(R.id.platform_list_view);
+        drawerList.setAdapter(new ArrayAdapter(this,  android.R.layout.simple_list_item_1, platformList));
+        drawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = null;
+                switch(i){
+                    case 0:
+                        intent = new Intent(getBaseContext(), ImageViewPlatform.class);
+                        break;
+                    case 1:
+                        intent = new Intent(getBaseContext(), LoggerPlatform.class);
+                        break;
+                }
+                if (intent != null) {
+                    startActivity(intent);
+                }
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
