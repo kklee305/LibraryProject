fab5f65bf7440515cd29c7fb1aa5bc0557a013bf
kklee305 <kklee305@gmail.com>
2015-05-30 18:41:15 -0400

    logger platform to test logger

---
+++ app/src/main/java/ca/kklee/libraryproject/LoggerPlatform/LoggerPlatform.java
@@ -0,0 +1,89 @@
+package ca.kklee.libraryproject.LoggerPlatform;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.kklee.utilities.Logger;
+
+import ca.kklee.libraryproject.BuildConfig;
+import ca.kklee.libraryproject.R;
+
+/**
+ * Created by Keith on 29/05/2015.
+ */
+public class LoggerPlatform extends Activity {
+
+    private Button verbose, debug, info, warning, error, wtf, viewLog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logger);
+
+        verbose = (Button) findViewById(R.id.test_v);
+        debug = (Button) findViewById(R.id.test_d);
+        info = (Button) findViewById(R.id.test_i);
+        warning = (Button) findViewById(R.id.test_w);
+        error = (Button) findViewById(R.id.test_e);
+        wtf = (Button) findViewById(R.id.test_wtf);
+        viewLog = (Button) findViewById(R.id.view_log_button);
+
+        //initLogger
+        Logger.setIsLogging(BuildConfig.DEBUG);
+        Logger.setLogToFile(getApplicationContext());
+
+        final TextView tv = (TextView) findViewById(R.id.log_view);
+        final SharedPreferences pref = getSharedPreferences(Logger.LOGGER_SHARED_PREF, Context.MODE_PRIVATE);
+        viewLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.setText(pref.getString(Logger.LOGGER_SHARED_PREF, ""));
+            }
+        });
+
+        createOnClickListener(verbose, "testing log verbose");
+        createOnClickListener(debug, "testing log debug");
+        createOnClickListener(info, "testing log info");
+        createOnClickListener(warning, "testing log warning");
+        createOnClickListener(error, "testing log error");
+        createOnClickListener(wtf, "testing log wtf");
+    }
+
+    private void createOnClickListener(final Button button, final String message) {
+        if (button == null) {
+            return;
+        }
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (button.getId()) {
+                    case R.id.test_v:
+                        Logger.v(message);
+                        break;
+                    case R.id.test_d:
+                        Logger.d(message);
+                        break;
+                    case R.id.test_i:
+                        Logger.i(message);
+                        break;
+                    case R.id.test_w:
+                        Logger.w(message);
+                        break;
+                    case R.id.test_e:
+                        Logger.e(message);
+                        break;
+                    case R.id.test_wtf:
+                        Logger.wtf(message);
+                        break;
+                }
+
+            }
+        });
+
+    }
+}
