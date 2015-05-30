0adf194c9cb6b6ddfc30a6432013c5a93978d9b7
kklee305 <kklee305@gmail.com>
2015-05-30 18:41:46 -0400

    ImageView platform (WIP!!!)

---
+++ app/src/main/java/ca/kklee/libraryproject/ImageViewPlatform/ImageViewPlatform.java
@@ -0,0 +1,18 @@
+package ca.kklee.libraryproject.ImageViewPlatform;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import ca.kklee.libraryproject.R;
+
+/**
+ * Created by Keith on 18/08/2014.
+ */
+public class ImageViewPlatform extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imageview);
+    }
+}
